e760992fa697b70d551f402ed95c836f8f3f246b
igor <igor.mayachenkov@hiq.ru>
2019-11-29 11:37:14 +0300

    production URL updated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import People from './People';
 import './App.css';
 
-global.API_URL = 'http://localhost:8080/api'; // DEVELOPMENT
-//global.API_URL = "http://http://192.168.21.41/api"; // PRODUCTION
+//global.API_URL = 'http://localhost:8080/api'; // DEVELOPMENT
+global.API_URL = "http://192.168.21.41/api"; // PRODUCTION
 
 function App() {
   return (
