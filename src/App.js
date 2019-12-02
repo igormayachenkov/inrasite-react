f8ef0b350a9a3e66b8e73f1cbe1690c66aa2b99b
igor <igor.mayachenkov@hiq.ru>
2019-12-02 18:09:05 +0300

    Router  added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import People from './People';
 import './App.css';
+import { NavLink, Route, Redirect } from 'react-router-dom';
+import Home from './Home';
+import People from './People';
 
 //global.API_URL = 'http://localhost:8080/api'; // DEVELOPMENT
 global.API_URL = "http://192.168.21.41/api"; // PRODUCTION
@@ -8,7 +10,15 @@ global.API_URL = "http://192.168.21.41/api"; // PRODUCTION
 function App() {
   return (
     <div>
-      <People />
+      <nav>
+      <NavLink exact to="/home" activeClassName='active'>Home</NavLink>
+      <NavLink to="/people" activeClassName='active'>People</NavLink>
+      </nav>
+      <div>
+      <Redirect from="/" exact to="/home" />
+      <Route path="/home" component={Home} />
+      <Route path="/people" component={People} />
+      </div>
     </div>
   );
 }
