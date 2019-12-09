58f617ca798809e4cf460ca00ce4e0dfbe23f63d
igor <igor.mayachenkov@hiq.ru>
2019-12-09 16:02:04 +0300

    People format updated

--- src/People.js
+++ src/People.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import './People.css';
 
+let month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
 class People extends Component {
   constructor(props) {
       super(props);
 
       // State
       this.state = {
-        people:[]
+        departments:{}
         //selectedId:null
       };
       // Handlers
@@ -29,9 +31,20 @@ class People extends Component {
         response.json()
           .then(data=>{
               if(response.status===200){
-                let state = {people:data};
+                let departments={}
+                data.forEach(p => {
+                  // Find/create department
+                  let name = p.pos_type;
+                  let d = departments[name];
+                  if(!d){
+                    d = []
+                    departments[name]=d;  
+                  }
+                  // Add to the department
+                  d.push(p)
+                });
                 // Update state
-                this.setState(state);
+                this.setState({departments:departments});
               }else{
                 console.error(data);
                 alert(data.message);
@@ -59,22 +72,36 @@ class People extends Component {
     console.log('Content will unmount');
   }    
   
+  renderPerson(p){
+    let position = p.pos_type!='developers'?p.position:null // remove position for developers
+    return (          
+      <div key={p.id} className='person'>
+      <div className='name'>{p.firstname} {p.lastname}</div>
+      <img className='photo' alt="photo"/>
+      <div className='position'>{position}</div>
+      <div className='phone'>{p.phone}</div>
+      <div className='mail'>{p.mail}</div>
+      <div className='birthday'>{month[p.birthday_month-1]+' '+p.birthday_day}</div>
+      </div>
+    )}
     
   render() {
-    console.log("People RENDER");
-    let items = this.state.people.map(p => 
-        <div key={p.id} className='person'>
-            <div className='name'>{p.firstname} {p.lastname}</div>
-            <img className='photo' alt="photo"/>
-            <div className='position'>{p.position}</div>
-            <div className='phone'>{p.phone}</div>
-            <div className='mail'>{p.mail}</div>
-            <div className='birthday'>{p.birthday_day+'.'+p.birthday_month}</div>
-        </div>
-    );
+    console.log("People RENDER", this.state.departments);
+    let dps = this.state.departments;
+    if(dps.administration)  var administration = dps.administration.map(this.renderPerson);
+    if(dps.developers)  var developers = dps.developers.map(this.renderPerson);
+    if(dps.testers)  var testers = dps.testers.map(this.renderPerson);
+    
     return (
-      <div>
-        {items}
+      <div className='people-page'>
+        <div className='header'>Administration</div>
+        <div className='block'>{administration}</div>
+
+        <div className='header'>Developers</div>
+        <div className='block'>{developers}</div>
+
+        <div className='header'>Testers</div>
+        <div className='block'>{testers}</div>
       </div>
     );
   }
