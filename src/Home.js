b5e1522af676dc9c28477346d13d7bfed5d157bc
igor <igor.mayachenkov@hiq.ru>
2019-12-31 13:07:36 +0300

    home updated to 2020

--- src/Home.js
+++ src/Home.js
@@ -11,25 +11,11 @@ function Home() {
       
       <div className="box">
         <h1>VACATIONS SCHEDULE</h1>
-        <a href={global.API_URL+'/files/documents/VacSchedule2019.xls'} download>2019 year vacations schedule</a>
+        <a href={global.API_URL+'/files/documents/VacSchedule2020.xls'} download>2020 year vacations schedule</a>
       </div>
 
       <div class="box">
         <h1>HOLIDAYS</h1>
-			1, 2, 3, 4, 5, 6, 8 января — Новогодние каникулы;<br/>     
-			7 января — Рождество Христово;<br/>     
-			23 февраля — День защитника Отечества;<br/>     
-			8 марта — Международный женский день;<br/>     
-			1 мая — Праздник Весны и Труда;<br/>     
-			9 мая — День Победы;<br/>     
-			12 июня — День России;<br/>     
-			4 ноября — День народного единства;<br/>     
-			<br/>
-			Постановлением Правительства Российской Федерации "О переносе выходных дней в 2015 году" от 27 августа 2014 г. № 860 предусмотрен перенос выходных дней:<br/>     
-			3 января (сб) → 9 января (пт) <br/>    
-			4 января (вс) → 4 мая (пн)	<br/>
-			<br/>
-			Для тех, кто не понял алгоритм, решение тут:<br/>
 			<a href="http://www.superjob.ru/proizvodstvennyj_kalendar/">Производственный календарь</a>
     </div>
 
